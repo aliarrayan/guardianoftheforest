63cc675f48860d7a34a2dad0bb8faec857ac27b4
unknown <maliarrayan@students.amikom.ac.id>
2025-07-07 18:01:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // ⛳ Tambahkan base path sesuai nama repo GitHub-mu!
 export default defineConfig({
-  base: '/guardianoftheforest/',
+  base: '/',
   plugins: [react()],
 })
