7ad2a39ffb1d26168972ab069c6afd286b9acfcc
unknown <maliarrayan@students.amikom.ac.id>
2025-07-07 17:17:34 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/guardianoftheforest/', // <--- WAJIB ini
   plugins: [react()],
-  base: '/guardianforest/', // ← tambahkan ini
 })
